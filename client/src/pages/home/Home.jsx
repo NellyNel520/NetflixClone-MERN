c702bffe64a3b54a52c334de34cdbc60a5d585dd
NellyNel520 <channelharris520@gmail.com>
2023-10-17 18:46:06 -0400

    created featured js file and scss file

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,6 +6,11 @@ const Home = () => {
   return (
     <div className='home'>
       <Navbar />
+      <img
+						alt=""
+            width="100%"
+						src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg"
+					/>
     </div>
   )
 }
