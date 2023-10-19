9defd7a3335bc1a2fcc7106f1916bb5921545c42
NellyNel520 <channelharris520@gmail.com>
2023-10-18 21:24:00 -0400

    completed auth router

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,11 @@
 const Router = require('express').Router()
+const AuthRouter = require("./AuthRouter")
 
 // Test
 Router.get('/', (req, res) => res.send('This is root babyyyyyyy!'))
 
+
+Router.use("/auth", AuthRouter)
 
 
 module.exports = Router;
