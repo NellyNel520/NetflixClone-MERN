4f058cae774458678301a7d04320d77f65c5b8b4
NellyNel520 <channelharris520@gmail.com>
2023-10-18 23:03:11 -0400

    completed movie router

---
+++ api/routes/MovieRouter.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const controller = require('../controllers/MovieController')
+const verify = require("../verifyToken");
+
+
+router.put('/:id', verify, controller.updateMovie)
+router.delete('/:id', verify, controller.deleteMovie)
+router.get('/find/:id', verify, controller.getMovieById)
+router.get('/random', verify, controller.getRandom)
+router.get('/', verify, controller.getAllMovies)
+
+
+module.exports = router
