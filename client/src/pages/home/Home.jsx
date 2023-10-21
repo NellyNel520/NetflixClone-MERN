b90ee84bfdce6570d52d416235ad67c902f3c8e4
NellyNel520 <channelharris520@gmail.com>
2023-10-21 19:49:13 -0400

    installed react redux,  redux toolkit and redux persist

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './home.scss'
 import Navbar from '../../components/navbar/Navbar'
 import Featured from '../../components/featured/Featured'
@@ -8,7 +9,21 @@ const Home = ({type}) => {
   const [lists, setLists] = useState([]);
   const [genre, setGenre] = useState(null);
 
-  
+  useEffect(() => {
+    const getRandomList = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`
+        ); 
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getRandomList()
+  }, [type, genre])
+
+
   return (
     <div className='home'>
       <Navbar />
