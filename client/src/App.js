c3106ebb54a8ed7748e5da3f5a6b35ce471b1244
NellyNel520 <channelharris520@gmail.com>
2023-10-21 18:18:06 -0400

    added conditional rendering to router based on if there is a user present or not

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,35 @@ import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch'
 import Signup from './pages/signup/Signup'
 import Login from './pages/login/Login'
-import { Routes, Route, Redirect } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
+	const user = false
 	return (
 		<Routes>
-			<Route path="/" exact element={<Home />} />
-      <Route path="/movies" element={<Home  type="movies"/>} />
-      <Route path="/series" element={<Home  type="series"/>} />
-      <Route path="/watch" element={<Watch />} />
+			<Route
+				path="/"
+				exact
+				element={user ? <Home /> : <Navigate to={'/signup'} />}
+			/>
+			<Route
+				path="/signup"
+				exact
+				element={!user ? <Signup /> : <Navigate to={'/'} />}
+			/>
+			<Route
+				path="/login"
+				exact
+				element={!user ? <Login /> : <Navigate to={'/'} />}
+			/>
+
+			{user && (
+				<>
+					<Route path="/movies" element={<Home type="movies" />} />
+					<Route path="/series" element={<Home type="series" />} />
+					<Route path="/watch" element={<Watch />} />
+				</>
+			)}
 		</Routes>
 	)
 }
