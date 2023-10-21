14654615271fa5da503a10df1f648eaf639b2e34
NellyNel520 <channelharris520@gmail.com>
2023-10-21 17:46:49 -0400

    created routes for home and watch screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
-import './App.scss';
-import Home from './pages/home/Home';
-import Watch from './pages/watch/Watch';
-import Signup from './pages/signup/Signup';
-import Login from './pages/login/Login';
-
+import './App.scss'
+import Home from './pages/home/Home'
+import Watch from './pages/watch/Watch'
+import Signup from './pages/signup/Signup'
+import Login from './pages/login/Login'
+import { Routes, Route, Redirect } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path="/" exact element={<Home />} />
+      <Route path="/movies" element={<Home  type="movies"/>} />
+      <Route path="/series" element={<Home  type="series"/>} />
+      <Route path="/watch" element={<Watch />} />
+		</Routes>
+	)
 }
 
-export default App;
+export default App
