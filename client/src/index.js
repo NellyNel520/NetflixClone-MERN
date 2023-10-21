47270a05b4a2c60302f7646d8cc01b5c93922238
NellyNel520 <channelharris520@gmail.com>
2023-10-20 22:56:39 -0400

    installed react router dom in client folder

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.scss'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode className='index'>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
