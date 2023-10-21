46b6e0a659c7a5fb61594598e0002da4ac77d3b1
NellyNel520 <channelharris520@gmail.com>
2023-10-21 17:56:29 -0400

    added link tags to nav bar for page navigation

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,30 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './navbar.scss'
 import SearchIcon from '@mui/icons-material/Search'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-	const [isScrolled, setIsScrolled] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false)
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
+	window.onscroll = () => {
+		setIsScrolled(window.pageYOffset === 0 ? false : true)
+		return () => (window.onscroll = null)
+	}
 
 	return (
-		<div className={isScrolled ? "navbar scrolled" : "navbar"}>
+		<div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
 			<div className="container">
 				<div className="left">
 					<img
 						alt=""
 						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
 					/>
-					<span>homepage</span>
-					<span>Series</span>
-					<span>Movies</span>
-					<span>New and Popular</span>
-					<span>My List</span>
+					<Link to={'/'} className='link'>
+						<span>Homepage</span>
+					</Link>
+					<Link to={"/series"} className='link'>
+						<span>Series</span>
+					</Link>
+					<Link to={"/movies"} className='link'>
+						<span>Movies</span>
+					</Link>
+					<Link className='link'>
+						<span>New and Popular</span>
+					</Link>
+					<Link className='link'>
+						<span>My List</span>
+					</Link>
 				</div>
 				<div className="right">
 					<SearchIcon className="icon" />
@@ -41,7 +52,6 @@ const Navbar = () => {
 							<span>Logout</span>
 						</div>
 					</div>
-          
 				</div>
 			</div>
 		</div>
