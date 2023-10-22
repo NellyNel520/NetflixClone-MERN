e2711f7d64649b13700da5ef86b2611e2a049d88
NellyNel520 <channelharris520@gmail.com>
2023-10-22 14:01:48 -0400

    added auth context provider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.scss'
 import { BrowserRouter } from 'react-router-dom'
-
+import { AuthContextProvider } from './authContext/AuthContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-	
+	<AuthContextProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</AuthContextProvider>
 )
